199d70c54d769e47cef1392fce5fdd7c26bf7624
lyr-glthub <75304664+lyr-glthub@users.noreply.github.com>
2023-03-23 21:01:19 +0800

    se

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup, find_packages
 setup(
-    name='zqwblog',
+    name='spin_up_blog',
     version='0.2',
     packages=find_packages(),
     description="Blog generator",
-    author='ZQW',
+    author='spin_up',
     author_email="zeqing6688@126.com",
 )
